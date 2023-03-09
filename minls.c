cb47c842f3c06828f9276b7ba3f7b480a51b721f
Joshua Kerley <joshuakerley116@gmail.com>
2023-03-09 11:11:36 -0800

    Implemented verbose mode

--- minls.c
+++ minls.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 #include "minutil.h"
 
 inode *getInode(int number, char *data)
@@ -58,7 +59,7 @@ dirent *getDirEntIndex(int index, inode *dir, char *data)
         }
 
         // TODO: read doubly indirect zones
-        printf("first-degree indirect zones exceeded! will be fixed later...\n");
+        printf("first-degree indirect zones exceeded!\n");
         exit(-1);
     }
 }
@@ -120,21 +121,27 @@ inode *findFile(char *path, char *data)
     return current;
 }
 
+/* Gets the permission string for a file */
+void *getPermissionString(int mode, char *modes)
+{
+    modes[0] = ((mode & 0170000) == 040000) ? 'd' : '-';
+    modes[1] = (mode & 0400) ? 'r' : '-';
+    modes[2] = (mode & 0200) ? 'w' : '-';
+    modes[3] = (mode & 0100) ? 'x' : '-';
+    modes[4] = (mode & 040) ? 'r' : '-';
+    modes[5] = (mode & 020) ? 'w' : '-';
+    modes[6] = (mode & 010) ? 'x' : '-';
+    modes[7] = (mode & 04) ? 'r' : '-';
+    modes[8] = (mode & 02) ? 'w' : '-';
+    modes[9] = (mode & 01) ? 'x' : '-';
+    modes[10] = '\0';
+}
+
 /* Prints the permissions and other info for a file */
 void printFileInfo(inode *file, char *name)
 {
     char modes[11];
-    modes[0] = isDirectory(file) ? 'd' : '-';
-    modes[1] = (file->mode & 0400) ? 'r' : '-';
-    modes[2] = (file->mode & 0200) ? 'w' : '-';
-    modes[3] = (file->mode & 0100) ? 'x' : '-';
-    modes[4] = (file->mode & 040) ? 'r' : '-';
-    modes[5] = (file->mode & 020) ? 'w' : '-';
-    modes[6] = (file->mode & 010) ? 'x' : '-';
-    modes[7] = (file->mode & 04) ? 'r' : '-';
-    modes[8] = (file->mode & 02) ? 'w' : '-';
-    modes[9] = (file->mode & 01) ? 'x' : '-';
-    modes[10] = '\0';
+    getPermissionString(file->mode, modes);
 
     printf("%s %9d %s\n", modes, file->size, name);
 }
@@ -223,7 +230,7 @@ int main(int argc, char **argv)
     char *filename = NULL;
     char *path = "/";
 
-    int v_flag = 0;
+    int verbose = 0;
     int usePartition = -1;
     int useSubpart = -1;
 
@@ -264,7 +271,7 @@ int main(int argc, char **argv)
             break;
         /* Verbose mode enabled */
         case 'v':
-            v_flag = 1;
+            verbose = 1;
             break;
         }
     }
@@ -335,8 +342,89 @@ int main(int argc, char **argv)
     /* Calculate zone size */
     int zonesize = sb->blocksize << sb->log_zone_size;
 
+    /* If verbose mode is enabled, print superblock info */
+    if(verbose)
+    {
+        printf("\nSuperblock Contents:\n"
+            "Stored Fields:\n"
+            "  ninodes \t%10d\n"
+            "  i_blocks \t%10d\n"
+            "  z_blocks \t%10d\n"
+            "  firstdata \t%10d\n"
+            "  log_zone_size %10d (zone size: %d)\n"
+            "  max_file \t%10u\n"
+            "  magic \t%10d\n"
+            "  zones \t%10d\n"
+            "  blocksize \t%10d\n"
+            "  subversion \t%10d\n\n",
+            sb->ninodes,
+            sb->i_blocks,
+            sb->z_blocks,
+            sb->firstdata,
+            sb->log_zone_size,
+            zonesize,
+            sb->max_file,
+            sb->magic,
+            sb->zones,
+            sb->blocksize,
+            sb->subversion);
+    }
+
     /* Get inode of file at path specified */
     inode *file = findFile(path, data);
+
+    /* If verbose mode is enabled, print file inode info */
+    if(verbose)
+    {
+        time_t at = file->atime;
+        time_t mt = file->mtime;
+        time_t ct = file->ctime;
+
+        char modes[11];
+        getPermissionString(file->mode, modes);
+
+        printf("File inode:\n"
+            "  uint16_t mode \t%#10x (%s)\n"
+            "  uint16_t links \t%10d\n"
+            "  uint16_t uid \t\t%10d\n"
+            "  uint16_t gid \t\t%10d\n"
+            "  uint32_t size \t%10d\n"
+            "  uint32_t atime \t%10d --- %s"
+            "  uint32_t mtime \t%10d --- %s"
+            "  uint32_t ctime \t%10d --- %s\n",
+            file->mode,
+            modes,
+            file->links,
+            file->uid,
+            file->gid,
+            file->size,
+            file->atime,
+            ctime(&at),
+            file->mtime,
+            ctime(&mt),
+            file->ctime,
+            ctime(&ct));
+
+        printf("Direct zones:\n"
+            "           zone[0]   = %10d\n"
+            "           zone[1]   = %10d\n"
+            "           zone[2]   = %10d\n"
+            "           zone[3]   = %10d\n"
+            "           zone[4]   = %10d\n"
+            "           zone[5]   = %10d\n"
+            "           zone[6]   = %10d\n"
+            "  uint32_t indirect    %10d\n"
+            "  uint32_t double      %10d\n",
+            file->zone[0],
+            file->zone[1],
+            file->zone[2],
+            file->zone[3],
+            file->zone[4],
+            file->zone[5],
+            file->zone[6],
+            file->indirect,
+            file->two_indirect);
+    }
 
     /* Make sure file was found */
     if (!file)
